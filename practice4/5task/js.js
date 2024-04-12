bb09752ec39d0778cfddd976e12c356150ac7c89
OsypovIV <144897051+OsypovIV@users.noreply.github.com>
2024-04-12 11:39:39 +0300

    Delete practice4 directory

--- practice4/5task/js.js
+++
@@ -1,8 +0,0 @@
-function toCamelCase(str) {
-    return str.replace(/-([a-z])/g, function(match, letter) {
-        return letter.toUpperCase();
-    });
-}
-
-console.log(toCamelCase("background-color")); // Вивід "backgroundColor"
-console.log(toCamelCase("list-style-image")); // Вивід "listStyleImage"
